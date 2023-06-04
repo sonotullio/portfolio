4e05fb856cdc3cb1f6e8e33ca1486c53f9e36ea7
Tullio Forneris <tullio@Tullios-MBP.lan>
2023-06-04 20:57:28 +0200

    added title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+          <title>Tullio Forneris | Full Stack Software Developer ✔</title>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
